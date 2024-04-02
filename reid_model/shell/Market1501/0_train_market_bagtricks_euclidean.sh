343834cf7ea1400711d04dd4fdca11b06c726c93
aapdo <jade04342@gmail.com>
2024-04-02 01:51:35 +0000

    change 0.train_market_bagtricks_euclidean.sh

--- reid_model/shell/Market1501/0_train_market_bagtricks_euclidean.sh
+++ reid_model/shell/Market1501/0_train_market_bagtricks_euclidean.sh
@@ -4,4 +4,4 @@ cd amd/reid_model
 #set gpus
 gpus='0'
 #train
-CUDA_VISIBLE_DEVICES=$gpus python3 ./tools/train_net.py  --config-file ./configs/Market1501/bagtricks_circle_R50.yml  MODEL.BACKBONE.PRETRAIN_PATH  '/root/amd/pretrain_models/market_circle_r50_ip.pth'  MODEL.DEVICE "cuda:0"   MODEL.BACKBONE.WITH_NL  False   TEST.METRIC   "euclidean"   TEST.EVAL_PERIOD 10  SOLVER.CHECKPOINT_PERIOD 10
+CUDA_VISIBLE_DEVICES=$gpus python3 ./tools/train_net.py  --config-file ./projects/InterpretationReID/configs/Market1501_Circle/circle_R50_ip.yml  MODEL.BACKBONE.PRETRAIN_PATH  '/root/amd/pretrain_models/market_circle_r50_ip.pth'  MODEL.DEVICE "cuda:0"   MODEL.BACKBONE.WITH_NL  False   TEST.METRIC   "euclidean"   TEST.EVAL_PERIOD 10  SOLVER.CHECKPOINT_PERIOD 10
