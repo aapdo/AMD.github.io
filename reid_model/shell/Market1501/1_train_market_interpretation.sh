92bb031617c3e3a8bfff0b9f7f05dbe82e277e5b
aapdo <jade04342@gmail.com>
2024-04-05 18:30:30 +0900

    move reid_model/projects/ipReid/ipreid/data/market1501_ip.py => fastreid/data & add dataset in __init__ Market1501_Interpretation

--- reid_model/shell/Market1501/1_train_market_interpretation.sh
+++ reid_model/shell/Market1501/1_train_market_interpretation.sh
@@ -1,6 +1,6 @@
 cd 
 cd amd/reid_model
 
-gpus='0,1,2'
+gpus='0, 1, 2'
 
-CUDA_VISIBLE_DEVICES=$gpus python3 ./projects/InterpretationReID/train_net.py  --config-file ./projects/InterpretationReID/configs/Market1501_Circle/circle_R50_ip.yml    MODEL.DEVICE "cuda:0"
+CUDA_VISIBEVICES=$gpus python3 ./projects/InterpretationReID/train_net.py  --config-file ./projects/InterpretationReID/configs/Market1501_Circle/circle_R50_ip.yml  --num-gpus 1  MODEL.DEVICE "cuda:0"
