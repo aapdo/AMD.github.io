fe9181abf2f7caf79204fe163ad5e597007bcac8
aapdo <jade04342@gmail.com>
2024-05-06 00:47:18 +0900

    add calculate different between original label and correct label

--- label_statistics/statistics.py
+++ label_statistics/statistics.py
@@ -5,6 +5,10 @@ import mat4py
 import re
 import os
 import csv
+import matplot
+
+cur_path = "./label_statistics"
+
 
 def get_all_files_in_folder(folder_path):
     """
@@ -46,23 +50,80 @@ def load_mat_attr(mat_file_path):
 
     return mat_attribute_train, mat_attribute_test, train_indexes, test_indexes
 
-def get_correct_origin_diff(xlsx_file_path):
+def get_correct_origin_diff(correct_label_df, mat_attribute_df):
     '''
     기존 라벨링 데이터와 수정한 라벨링 데이터 사이에서 얼마나 수정되었는지 계산하는 함수
     '''
-    df = pd.read_excel(xlsx_file_path)
-    
-    for index, row in df.iterrows():
-        print(1)
+    #pattern = r'\d{4}' 
+    pattern = r'^.{4}'
+    # 전체 라벨 수
+    total_label_number = 0
+    # 하나라도 라벨이 수정된 사진의 수
+    correct_img_number = 0
+    # 각 속성별로 수정된 수
+    cnt_correct_attr = {
+        'age': 0,
+        'backpack': 0,
+        'bag': 0,
+        'handbag': 0,
+        'downblack': 0,
+        'downblue': 0,
+        'downbrown': 0,
+        'downgray': 0,
+        'downgreen': 0,
+        'downpink': 0,
+        'downpurple': 0,
+        'downwhite': 0,
+        'downyellow': 0,
+        'upblack': 0,
+        'upblue': 0,
+        'upgreen': 0,
+        'upgray': 0,
+        'uppurple': 0,
+        'upred': 0,
+        'upwhite': 0,
+        'upyellow': 0,
+        'clothes': 0,
+        'down': 0,
+        'up': 0,
+        'hair': 0,
+        'hat': 0,
+        'gender': 0,        
+    }
 
+    for index, row in correct_label_df.iterrows():
+        total_label_number += 1
 
-    return df
+        img_name = row["origin_img_name"]
+        # 현재 확인하는 이미지의 식별 번호
+        pid = re.match(pattern, img_name).group()
+        # 기존 라벨 속성
+        original_attr = mat_attribute.loc[pid]
+        # 수정이 한번이라도 이루어졌는지 체크하는 변수
+        flag = False
 
+        for key, value in original_attr.items():
+            if value != row[key]:
+                cnt_correct_attr[key] += 1
+                flag = True
+        if flag:
+            correct_img_number += 1
+
+    return total_label_number, correct_img_number, cnt_correct_attr
+
+def draw_graph(total_label_number, correct_img_number, cnt_correct_attr):
+    '''
+    2. 전체 라벨 중 수정한 비율이 얼마나 되는지
+    3. 수정 후 속성 분포가 어떻게 되는지
+    4. 각 속성별로 수정된 비율이 얼마나 되는지
+    '''
+
+    return
 
 if __name__ == '__main__':
-    mat_file_path = "./market_attribute.mat"
-    correct_label_file_path = "./correct_label.xlsx"
-    img_file_path = "./labelTarget"
+    mat_file_path = cur_path + "/market_attribute.mat"
+    correct_label_file_path = cur_path + "/correct_label.xlsx"
+    img_file_path = cur_path + "/labelTarget"
 
     # 정규 표현식
     
@@ -77,8 +138,13 @@ if __name__ == '__main__':
             writer.writerow([name])
     '''
 
-    load_xlsx_label(correct_label_file_path)
+    correct_label_df = pd.read_excel(correct_label_file_path)
+    mat_attribute_train, mat_attribute_test, train_indexes, test_indexes = load_mat_attr(mat_file_path)
 
-        
+    # train + test attribute
+    mat_attribute = pd.concat([mat_attribute_train, mat_attribute_test], axis=0).sort_index() 
+    mat_attribute = mat_attribute.drop(columns=['image_index'])
 
-    mat_attribtue_train, mat_attribtue_test, train_indexes, test_indexes = load_mat_attr(mat_file_path)
+    total_label_number, correct_img_number, cnt_correct_attr = get_correct_origin_diff(correct_label_df, mat_attribute)
+
+    
