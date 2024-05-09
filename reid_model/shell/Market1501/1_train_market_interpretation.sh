524a930d30673a7f2d50fd25b27afe418388d45c
aapdo <jade04342@gmail.com>
2024-05-09 22:31:33 +0900

    update setting

--- reid_model/shell/Market1501/1_train_market_interpretation.sh
+++ reid_model/shell/Market1501/1_train_market_interpretation.sh
@@ -5,3 +5,4 @@ gpus='0'
 
 # ps -ef | grep "python3 ./projects/InterpretationReID" | awk '{print $2}' | xargs kill
 CUDA_VISIBEVICES=$gpus python3 ./projects/InterpretationReID/train_net.py  --config-file ./projects/InterpretationReID/configs/Market1501_Circle/circle_R50_ip.yml  MODEL.DEVICE "cuda:0"
+# CUDA_VISIBEVICES='0' --config-file ./projects/InterpretationReID/configs/Market1501_Circle/circle_R50_ip.yml  MODEL.DEVICE "cuda:0"
